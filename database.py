b4c426f326458d942db71734f73e9f514edd6167
ismo23267-bit <ismo23267@gmail.com>
2026-03-20 23:29:09 +0600

    switch to postgres on render

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./lang_app.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./lang_app.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(
     autocommit=False,
